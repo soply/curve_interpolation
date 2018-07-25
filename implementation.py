4fe19172272afea8a21ae1e8976e7e0a8aa8bcbc
Timo Klock <timo@Timos-MacBook-Pro.local>
2018-07-25 15:01:52 +0200

    Some things

--- implementation.py
+++ implementation.py
@@ -3,22 +3,24 @@ Implementation of an Algorithm in order to reconstruct at curve from a
 scattered set of points with orthogonal error.
 
 Based on the Master's thesis of Johannes Piendl "Learning a Manifold from
-Functional Data in Low Dimension". 
+Functional Data in Low Dimension".
 """
 
 
 # Imports
-import matplotlib.pyplot as plt 
+import time
+import matplotlib.pyplot as plt
 import numpy as np
 import numpy.linalg as npl
 import pylab as pl
 import scipy.sparse.linalg as spl
 from scipy import sparse
-
+from sklearn.decomposition import PCA
+from partitioning.based_on_domain import create_partitioning_n
 
 
 def reconstructCurve(t0, t1, points, diffpoints, dom, N,numberOfLevels, lam=1):
-	
+
 	"""
 	Parameters
 	==================
@@ -28,10 +30,10 @@ def reconstructCurve(t0, t1, points, diffpoints, dom, N,numberOfLevels, lam=1):
 
 	N: integer
 		Number of partitions
-	
+
 	points: np.array of floats, size 2 x M
 		Scattered points around the curve with orthogonal error.
-	
+
 	diffpoints: np.array of floats, size 2 x M
 		The values of the derivatives of the curve at each partition.
 
@@ -44,18 +46,18 @@ def reconstructCurve(t0, t1, points, diffpoints, dom, N,numberOfLevels, lam=1):
 
 	Returns
 	==================
-	
+
 	y: np.array of floats, size 2 x M
 		The optimal minimalization of the distance function.
-	
+
 	means: np.array of floats, size 2 x M
 		The mean points of each level set.
 
-	z: np.array of floats, size M x 2 
+	z: np.array of floats, size M x 2
 		a vector that spans the set of solutions for the minimalization
 		problem. All solutions are on the form y + t*z, where t is a real
-		number. 
-	
+		number.
+
 	L: np.array of floats, size M x numberOfPoints x 2
 		The level sets.
 
@@ -75,86 +77,101 @@ def reconstructCurve(t0, t1, points, diffpoints, dom, N,numberOfLevels, lam=1):
 	L = np.zeros((numberOfLevels,points[0].size,2))
 	means = np.zeros((2,numberOfLevels))
 
+
+	labels = np.zeros(N).astype('int')
+	labels, edges = create_partitioning_n(dom, numberOfLevels)
 	for i in range(numberOfLevels):
-		count = 0
-		for p in range(N):
-			if b[i] <= dom[p] and b[i+1] > dom[p]:
-				L[i][count] = [points[0][p],points[1][p]]
-				count = count +1
-		
-		x_mean,y_mean = 0,0
-		for k in range(count):
-			x_mean += L[i][k][0]
-			y_mean += L[i][k][1]
-		
-		
-		if count != 0:
-			means[0][i] = float(x_mean)/count 
-			means[1][i] = float(y_mean)/count
+		means[:,i] = np.mean(points[:,labels == i+1], axis = 1)
 
-
-
-	"""	
+	# start = time.time()
+	# for i in range(numberOfLevels):
+	# 	count = 0
+	# 	for p in range(N):
+	# 		if b[i] <= dom[p] and b[i+1] > dom[p]:
+	# 			L[i][count] = [points[0][p],points[1][p]]
+	# 			count = count +1
+	#
+	# 	x_mean,y_mean = 0,0
+	# 	for k in range(count):
+	# 		x_mean += L[i][k][0]
+	# 		y_mean += L[i][k][1]
+	#
+	#
+	# 	if count != 0:
+	# 		means[0][i] = float(x_mean)/count
+	# 		means[1][i] = float(y_mean)/count
+	# timenew = time.time()
+	#
+	# print 'Loop 1: ', timenew - start
+	# start = timenew
+	"""
 	Helping function, returns the number of points in a given level set.
 	"""
-	
-
-	def getNumberOfPointsInLevel(i):	
-		c = 0
-		while L[i][points[0].size-1-c][0]==0 and L[i][points[0].size-1-c][1]==0:
-			c += 1
-	
-		return points[0].size - c
 
 
+	def getNumberOfPointsInLevel(i):
+		return len(np.where(labels == i+1)[0])
+		# c = 0
+		# while L[i][points[0].size-1-c][0]==0 and L[i][points[0].size-1-c][1]==0:
+		# 	c += 1
+		#
+		# return points[0].size - c
+	#
+	# timenew = time.time()
+	# print 'Loop 2: ', timenew - start
+	# start = timenew
 
 	"""
 	Step 2: Finding the avarage normal vector for each level set.
 	"""
-
-	N_list = []		
+	pca = PCA()
+	N_list = []
 
 	for j in range(numberOfLevels):
-		
-		shiftedPoints = np.zeros((getNumberOfPointsInLevel(j),2))
-		P = np.zeros((getNumberOfPointsInLevel(j),2))
+		pca = pca.fit(points[:,labels == j+1].T)
+		n = pca.components_[0,:]
+		# shiftedPoints = np.zeros((getNumberOfPointsInLevel(j),2))
+		# P = np.zeros((getNumberOfPointsInLevel(j),2))
+		#
+		# for i in range(getNumberOfPointsInLevel(j)):
+		# 	shiftedPoints[i][0] = L[j][i][0] - means[0][j]
+		# 	shiftedPoints[i][1] = L[j][i][1] - means[1][j]
+		#
+		# 	P[i][0] = shiftedPoints[i][0]
+		# 	P[i][1] = shiftedPoints[i][1]
+		#
+		# u,s,v = npl.svd(P)
+		# n = v[0]
 
-		for i in range(getNumberOfPointsInLevel(j)):
-			shiftedPoints[i][0] = L[j][i][0] - means[0][j]
-			shiftedPoints[i][1] = L[j][i][1] - means[1][j]
-		
-			P[i][0] = shiftedPoints[i][0]
-			P[i][1] = shiftedPoints[i][1]
-
-		u,s,v = npl.svd(P) 
-		n = v[0]	
-		
 		N_list.append(n)
-	
 
-	
+	# timenew = time.time()
+	# print 'Loop 3: ', timenew - start
+	# start = timenew
+
+
+
 	"""
-	Step 3: Setting up the distance function and minimalizing it. 
+	Step 3: Setting up the distance function and minimalizing it.
 
 	"""
 
-	def dist(y):
-		s = 0
-		for j in range(numberOfLevels):
-			s += abs(y[2*j]-means[0][j])**2 + abs(y[2*j+1] - means[1][j])**2\
-				-((y[2*j]-means[0][j])*N_list[j][0]\
-				+(y[2*j+1]-means[1][j])*N_list[j][1])**2 
-		for j in range(numberOfLevels-1):
-			s += lam*((y[2*j+2]-y[2*j])*N_list[j][0]\
-				+ (y[2*j+3]-y[2*j+1])*N_list[j][1])**2 
-		return s
-
+	# def dist(y):
+	# 	s = 0
+	# 	for j in range(numberOfLevels):
+	# 		s += abs(y[2*j]-means[0][j])**2 + abs(y[2*j+1] - means[1][j])**2\
+	# 			-((y[2*j]-means[0][j])*N_list[j][0]\
+	# 			+(y[2*j+1]-means[1][j])*N_list[j][1])**2
+	# 	for j in range(numberOfLevels-1):
+	# 		s += lam*((y[2*j+2]-y[2*j])*N_list[j][0]\
+	# 			+ (y[2*j+3]-y[2*j+1])*N_list[j][1])**2
+	# 	return s
 
 	n = numberOfLevels
 
 	diag = np.zeros((n,2,2))
 	upper_diag = np.zeros((n-1,2,2))
-	lower_diag = np.zeros((n-1,2,2))	
+	lower_diag = np.zeros((n-1,2,2))
 
 	diag[0] = np.identity(2) - np.outer(N_list[0],N_list[0])\
 				+lam*np.outer(N_list[0],N_list[0])
@@ -168,34 +185,41 @@ def reconstructCurve(t0, t1, points, diffpoints, dom, N,numberOfLevels, lam=1):
 	for j in range(n-1):
 		upper_diag[j] = -lam*np.outer(N_list[j],N_list[j])
 		lower_diag[j] = -lam*np.outer(N_list[j],N_list[j])
-	 
 
-	A = sparse.bmat([[diag[i] if i == j 
-						else upper_diag[j] if i-j == 1 
-						else lower_diag[i] if i-j == (-1)  
-						else None for i in range(n)] 
+
+	A = sparse.bmat([[diag[i] if i == j
+						else upper_diag[j] if i-j == 1
+						else lower_diag[i] if i-j == (-1)
+						else None for i in range(n)]
 							for j in range(n)], format = 'csc')
-
+	# timenew = time.time()
+	# print 'Creating A: ', timenew - start
+	# start = timenew
 
 	b = np.zeros(2*n)
 
 	for j in range(1,n+1):
 		temp1 = np.identity(2) - np.outer(N_list[j-1],N_list[j-1])
-		temp2 = np.array([means[0][j-1],means[1][j-1]]) 
+		temp2 = np.array([means[0][j-1],means[1][j-1]])
 		b[2*j-2:2*j] = np.matmul(temp1,temp2)
-	
+
+	# timenew = time.time()
+	# print 'Creating b: ', timenew - start
+	# start = timenew
 
 	ymin = spl.spsolve(A,b)
-	
 
+	# timenew = time.time()
+	# print 'Solving lin sts: ', timenew - start
+	# start = timenew
 
 	"""
 	The minimalization of the distance funtion is not uniqe, all solutions are
-	on the for ymin + t*z, where z is a vector in the kernel of A. 
-	
+	on the for ymin + t*z, where z is a vector in the kernel of A.
+
 	Settin up a vector that is in the kernel of A.
 	"""
-	
+
 	mu = np.zeros(numberOfLevels)
 	z = np.zeros((numberOfLevels,2))
 
@@ -207,8 +231,10 @@ def reconstructCurve(t0, t1, points, diffpoints, dom, N,numberOfLevels, lam=1):
 		 			+ N_list[j-1][1]*N_list[j][1])*mu[j-1]
 		z[j] = mu[j]*N_list[j]
 
+	# timenew = time.time()
+	# print 'Loop 4: ', timenew - start
+	# start = timenew
 
-	
 	"""
 	Finding the optimalized t-value.
 	"""
@@ -219,17 +245,20 @@ def reconstructCurve(t0, t1, points, diffpoints, dom, N,numberOfLevels, lam=1):
 
 		prod = z[i][0]*ymin[2*i] + z[i][1]*ymin[2*i+1]
 
-		temp = 0
-		for k in range(getNumberOfPointsInLevel(i)):
-			temp += z[i][0]*L[i][k][0] + z[i][1]*L[i][k][1]
-		
-		t_opt += 1.0/(getNumberOfPointsInLevel(i))*temp - prod
-		denom += npl.norm(z[i])**2	
-	
+		temp = z[i].dot(means[:,i])
+		# for k in range(getNumberOfPointsInLevel(i)):
+		# 	temp += z[i][0]*L[i][k][0] + z[i][1]*L[i][k][1]
+
+		t_opt += temp - prod
+		denom += npl.norm(z[i])**2
+
 	t_opt = t_opt/denom
 
+	# timenew = time.time()
+	# print 'Loop 5: ', timenew - start
+	# start = timenew
 
-	
+
 	"""
 	Returning the solution.
 	"""
@@ -240,7 +269,7 @@ def reconstructCurve(t0, t1, points, diffpoints, dom, N,numberOfLevels, lam=1):
 		y[0][i] = ymin[2*i] + t_opt*z[i][0]
 		y[1][i] = ymin[2*i+1] + t_opt*z[i][1]
 
-	return y, means, z, L, N_list
-
-
-
+	# timenew = time.time()
+	# print 'Loop 6: ', timenew - start
+	# start = timenew
+	return y, means, z, labels, N_list
--- test_implementation.py
+++ test_implementation.py
@@ -4,16 +4,15 @@ import sys
 import os
 import numpy as np
 from implementation import reconstructCurve
-from joblib import Parallel, delayed, dump, load 
+from joblib import Parallel, delayed, dump, load
 import matplotlib.pyplot as plt
-#from multiprocessing import Process, JoinableQueue 
+#from multiprocessing import Process, JoinableQueue
 import problem_factory.curve_classes as cc
 from problem_factory.sample_synthetic_data import sample_fromClass
-
-
+from visualisation.vis_nD import *
 
 def error_test(original_curve, t0 , t1, Np,numberOfLevels,lam):
-	
+
 	"""
 	Function that estimates the error between the original curve and the
 	reconstructed curve.
@@ -24,28 +23,29 @@ def error_test(original_curve, t0 , t1, Np,numberOfLevels,lam):
 
 	if isNotValidRun():
 		return
-
-
 	dom,curve,coeff,x,tan,norm = sample_fromClass(t0,t1,original_curve,Np,0.2)
-	ymin,means,z,L,N = reconstructCurve(t0,t1,x,tan,dom, Np,numberOfLevels,lam)
+	ymin,means,z,labels,N = reconstructCurve(t0,t1,x,tan,dom, Np,numberOfLevels,lam)
 
 
 	"""
 	Helping function that calculates the projection onto the average tangent
 	vector of a given level set.
 	"""
+	tangents_per_levelset = np.zeros((2,numberOfLevels))
+	for i in range(numberOfLevels):
+		tangents_per_levelset[:,i] = np.array([(-1)*N[i][1],N[i][0]])
 
 	def proj(point,i):
 		p = [0,0]
 		tan = np.array([(-1)*N[i][1],N[i][0]])
 		inner_prod = tan[0]*(point[0]-ymin[0][i]) + tan[1]*(point[1]-ymin[1][i])
-		
+
 		p[0] = ymin[0][i] + inner_prod*tan[0]
 		p[1] = ymin[1][i] + inner_prod*tan[1]
 
 		return p
 
-	
+
 	"""
 	Calculation of the error.
 	"""
@@ -53,32 +53,33 @@ def error_test(original_curve, t0 , t1, Np,numberOfLevels,lam):
 	count = 0
 
 	for i in range(numberOfLevels):
-		c = 0
-		while L[i][x[0].size-1-c][0]==0 and L[i][x[0].size-1-c][1]==0:
-			c += 1
+		projections = (ymin[:,i] + np.outer(tangents_per_levelset[:,i], (tangents_per_levelset[:,i])).dot((x[:,labels == i+1].T - ymin[:,i]).T).T).T
+		err += np.sum(np.square(np.linalg.norm(projections - curve[:,labels == i+1], axis = 1)))
+		# c = 0
+		# while L[i][x[0].size-1-c][0]==0 and L[i][x[0].size-1-c][1]==0:
+		# 	c += 1
+		#
+		# trunk_value = x[0].size - c
+		#
+		# for k in range(trunk_value):
+		# 	err += abs(curve[0][count] - proj(L[i][k],i)[0])**2
+		# 	err += abs(curve[1][count] - proj(L[i][k],i)[1])**2
+		# 	count += 1
 
-		trunk_value = x[0].size - c
-
-		for k in range(trunk_value):
-			err += abs(curve[0][count] - proj(L[i][k],i)[0])**2 
-			err += abs(curve[1][count] - proj(L[i][k],i)[1])**2 
-			count += 1	
-	
 	err = 1.0/(x[0].size)*err
-
 	sys.stdout = open('results.txt','a')
 
 	print('Error value:', err,'\n',
-			'with lambda: ', lam, ', Number of level sets: ', 
+			'with lambda: ', lam, ', Number of level sets: ',
 						numberOfLevels, ' and number of points: ', Np,'\n')
 
-	"""	
+	"""
 	q.put('Error value: ' + str(err) + '\n' + \
 			'with lambda: ' + str(lam) + ', Number of level sets: ' \
 						+ str(numberOfLevels) + ' and number of points: '\
 													+ str(Np) + '\n' + '\n')
 	"""
-	
+
 	return err
 
 
@@ -89,7 +90,6 @@ def visual_test(original_curve, t0 , t1, Np,numberOfLevels,lam):
 	Fuction that plots the original curve, the means of each level
 	set and the reconstructed curve.
 	"""
-
 	def isNotValidRun():
 		return Np <= numberOfLevels
 
@@ -98,7 +98,7 @@ def visual_test(original_curve, t0 , t1, Np,numberOfLevels,lam):
 
 
 	dom,curve,coeff,x,tan,norm = sample_fromClass(t0,t1,original_curve,Np,0.2)
-	ymin,means,z,L,N = reconstructCurve(t0,t1,x,tan,dom, Np,numberOfLevels,lam)
+	ymin,means,z,labels,N = reconstructCurve(t0,t1,x,tan,dom, Np,numberOfLevels,lam)
 
 	fig, ax = handle_2D_plot()
 
@@ -109,14 +109,14 @@ def visual_test(original_curve, t0 , t1, Np,numberOfLevels,lam):
 	plt.show()
 
 
-def saver(q):	
+def saver(q):
 	with open('results.txt','w') as out:
 		while True:
 			val = q.get()
 
 			if val is None:
 				break
-			
+
 			out.write(val)
 			q.task_done()
 		q.task_done()
@@ -131,12 +131,15 @@ if __name__ == '__main__':
 	"""
 
 	start_time = time.time()
-	
+
 	circle = cc.Circle_Piece_2D(2)
 
-	lam = [0.1,1,10,1000]
-	levels = [5,10,50,100]
+	lam = [1000]
+	levels = [10, 20, 40, 80, 160]
 
+    dummy_for_shape = np.zeros((len(lam), len(levels))
+    err = np.memmap(os.path.join(tmp_folder, 'err'), dtype='float64',
+                       shape=dummy_for_shape.shape, mode='w+')
 
 	"""
 	q = mp.JoinableQueue()
@@ -146,37 +149,36 @@ if __name__ == '__main__':
 	"""
 	out = open('results.txt','w')
 	out.close()
-		
-	err = Parallel(n_jobs=2)(delayed(error_test) 
+
+	err = Parallel(n_jobs=1)(delayed(error_test)
 			(circle,0,1.4,50*lev,lev,l) for lev in levels for l in lam)
 
-	
+
 	res = open('results.txt','r')
-	
+
 	error = []
 	lamb = []
 	num_points = []
 
 	for line in res:
 		arr = line.split(' ')
-		
+
 		if len(arr) == 4:
 			error.append(float(arr[2]))
 
 		elif len(arr) == 20:
 			lamb.append(float(arr[4]))
 			num_points.append(float(arr[18]))
-	
 
 	plt.plot(num_points,error,'ro')
 	plt.xlabel('Number of points')
 	plt.ylabel('Error')
 	plt.show()
 
-		
-	"""	
+
+	"""
 	q.put(None)
 	p.join()
 	"""
 	plt.show()
-	print('time:', (time.time()-start_time))
+	# print('time:', (time.time()-start_time))
